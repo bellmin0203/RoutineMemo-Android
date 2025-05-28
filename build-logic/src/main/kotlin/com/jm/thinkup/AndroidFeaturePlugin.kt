30e5640226c20eec979c6605464336d3980509d0
한종민 <jongm0203@gmail.com>
2025-05-28 16:41:31 +0900

    feature plugin에 일부 core 모듈 의존성 추가

--- build-logic/src/main/kotlin/com/jm/thinkup/AndroidFeaturePlugin.kt
+++ build-logic/src/main/kotlin/com/jm/thinkup/AndroidFeaturePlugin.kt
@@ -23,6 +23,10 @@ class AndroidFeaturePlugin : Plugin<Project> {
             dependencies {
                 "implementation"(project(":core:ui"))
                 "implementation"(project(":core:designsystem"))
+                "implementation"(project(":core:model"))
+                "implementation"(project(":core:data"))
+                "implementation"(project(":core:domain"))
+                "implementation"(project(":core:navigation"))
 
                 "implementation"(libs.findLibrary("androidx.hilt.navigation.compose").get())
                 "implementation"(libs.findLibrary("androidx.lifecycle.runtime.compose").get())

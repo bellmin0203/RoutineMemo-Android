34df744649379fe3f4c54a91cd48761ee24c60ce
한종민 <jongm0203@gmail.com>
2025-06-02 14:07:53 +0900

    Feat: 액션(Action) 모델 repository 구현

--- core/data/src/main/kotlin/com/jm/thinkup/data/repository/ActionsRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/jm/thinkup/data/repository/ActionsRepositoryImpl.kt
@@ -1,6 +1,43 @@
 package com.jm.thinkup.data.repository
 
+import com.jm.thinkup.database.dao.ActionDao
+import com.jm.thinkup.database.model.toDomainModel
+import com.jm.thinkup.database.model.toEntity
+import com.jm.thinkup.domain.model.ActionData
+import com.jm.thinkup.domain.model.GoalId
+import com.jm.thinkup.domain.model.ObstacleId
 import com.jm.thinkup.domain.repository.ActionsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class ActionsRepositoryImpl : ActionsRepository {
+class ActionsRepositoryImpl constructor(
+    private val actionDao: ActionDao
+) : ActionsRepository {
+    override suspend fun createAction(action: ActionData) {
+        actionDao.insertAction(action = action.toEntity())
+    }
+
+    override suspend fun getActionsByGoalId(goalId: GoalId): Flow<List<ActionData>> {
+        return actionDao.getActionsByGoalId(goalId = goalId.value).map { actionEntityList ->
+            actionEntityList.map { actionEntity ->
+                actionEntity.toDomainModel()
+            }
+        }
+    }
+
+    override suspend fun getActionsByObstacleId(obstacleId: ObstacleId): Flow<List<ActionData>> {
+        return actionDao.getActionsByObstacleId(obstacleId.value).map { obstacleEntityList ->
+            obstacleEntityList.map { obstacleEntity ->
+                obstacleEntity.toDomainModel()
+            }
+        }
+    }
+
+    override suspend fun updateAction(action: ActionData) {
+        actionDao.updateAction(action = action.toEntity())
+    }
+
+    override suspend fun deleteAction(action: ActionData) {
+        actionDao.deleteAction(action = action.toEntity())
+    }
 }
--- core/domain/src/main/kotlin/com/jm/thinkup/domain/repository/ActionsRepository.kt
+++ core/domain/src/main/kotlin/com/jm/thinkup/domain/repository/ActionsRepository.kt
@@ -1,4 +1,18 @@
 package com.jm.thinkup.domain.repository
 
+import com.jm.thinkup.domain.model.ActionData
+import com.jm.thinkup.domain.model.GoalId
+import com.jm.thinkup.domain.model.ObstacleId
+import kotlinx.coroutines.flow.Flow
+
 interface ActionsRepository {
+    suspend fun createAction(action: ActionData)
+
+    suspend fun getActionsByGoalId(goalId: GoalId): Flow<List<ActionData>>
+
+    suspend fun getActionsByObstacleId(obstacleId: ObstacleId): Flow<List<ActionData>>
+
+    suspend fun updateAction(action: ActionData)
+
+    suspend fun deleteAction(action: ActionData)
 }

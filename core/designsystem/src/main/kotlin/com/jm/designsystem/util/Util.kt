85f930ee0c08e3352f551cfa3fd0d57ecf95a596
한종민 <jongm0203@gmail.com>
2025-06-05 16:20:41 +0900

    Feat: Compose UI Util Class 추가

---
+++ core/designsystem/src/main/kotlin/com/jm/designsystem/util/Util.kt
@@ -0,0 +1,7 @@
+package com.jm.designsystem.util
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+fun Modifier.cardDefaultPadding(): Modifier = this.padding(horizontal = 16.dp, vertical = 8.dp)

4ed07c8b955804a81f040b06838826d41883db78
한종민 <jongm0203@gmail.com>
2025-06-20 16:44:05 +0900

    HotFix: MainActivity Hilt Entry Point 등록이 안되어 있어 런타임 에러 문제 수정

--- app/src/main/kotlin/com/jm/thinkup/MainActivity.kt
+++ app/src/main/kotlin/com/jm/thinkup/MainActivity.kt
@@ -3,13 +3,15 @@ package com.jm.thinkup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import com.jm.thinkup.ui.ThinkUpApp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             ThinkUpApp()
         }
